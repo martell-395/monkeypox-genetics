4165d8bfbc2cacfc889eb1a1336f8864a84c12c9
Samantha Martell <martell.samantha.j@gmail.com>
2022-05-29 20:19:20 +0100

    inital commit

---
+++ Monkeypox genetics.R
@@ -0,0 +1,11 @@
+#
+# The genetics of monkeypox
+# Tutorial by Dr. Danny Arends
+#
+# Samantha Martell
+# File opened: 29 May 2022
+# Final edit:
+# 
+
+# set working directory
+setwd("C:/Users/samma/Monkeypox-genetics")
