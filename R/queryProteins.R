010f0cf6920f220f8921eb4af5aa3bf484e2fce2
Samantha Martell <martell.samantha.j@gmail.com>
2022-06-03 17:13:24 +0100

    Finished Part 3.

---
+++ R/queryProteins.R
@@ -0,0 +1,16 @@
+# ID is the the ID of the (annotated) nucleotide sequence in the NCBI database.
+
+queryProteins <- function(id = "NC_003310.1"){ 
+  links <- entrez_link(dbfrom = "nuccore", id = id, db = "protein")
+  protein.ids <- links$links$nuccore_protein
+  
+  cat("This list has", length(protein.ids), "entries. \n")
+  # cat("The class of protein.ids is", class(protein.ids), ".")
+  
+  # unannotated nucleotide sequences will produce a protein.ids list with a length of 0, so don't want to query those. 
+  if(length(protein.ids) > 0){
+    protein.seq <- downloadData(protein.ids, "protein")
+    return(protein.seq)
+  }
+  return(NULL)
+}
